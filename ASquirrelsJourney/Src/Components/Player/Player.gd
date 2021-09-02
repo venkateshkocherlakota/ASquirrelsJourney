79a8a8bc52d1ae9766c84fedbc6ce41b13e3920e
unknown <kocherlakotavenkatesh666@gmail.com>
2021-09-02 10:37:07 +0530

    Jimmy basic done

---
+++ ASquirrelsJourney/Src/Components/Player/Player.gd
@@ -0,0 +1,38 @@
+extends KinematicBody2D
+
+
+export var speed = 50
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	
+	var movec = Vector2.ZERO
+	var moving = false
+	
+	if Input.is_action_pressed("ui_down"):
+		movec.y += 1
+		moving = true
+	elif Input.is_action_pressed("ui_up"):
+		movec.y -= 1
+		moving = true
+	
+	if Input.is_action_pressed("ui_right"):
+		movec.x += 1
+		moving = true
+	elif Input.is_action_pressed("ui_left"):
+		movec.x -= 1
+		moving = true
+
+	movec = movec.normalized() * speed * delta
+	
+	move_and_slide(movec, Vector2.UP)
+	
+	if moving == true:
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
