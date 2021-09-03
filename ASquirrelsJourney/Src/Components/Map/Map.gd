1ef7b3f65c10eff698010eddff55ad2dda6dc0ec
unknown <kocherlakotavenkatesh666@gmail.com>
2021-09-03 17:06:49 +0530

    Map completed

--- ASquirrelsJourney/Src/Components/Map/Map.gd
+++ ASquirrelsJourney/Src/Components/Map/Map.gd
@@ -8,8 +8,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$OrangeYellowLeaves.emitting = true
-	$PinkOrangeLeaves.emitting = true
+	$OYLeaves/OrangeYellowLeaves.emitting = true
+	$OYLeaves/OrangeYellowLeaves2.emitting = true
+	$OYLeaves/OrangeYellowLeaves3.emitting = true
+	$POLeaves/PinkOrangeLeaves.emitting = true
+	$POLeaves/PinkOrangeLeaves2.emitting = true
+	$POLeaves/PinkOrangeLeaves3.emitting = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
